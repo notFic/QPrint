100f117a5d0b97ce04a67ba888b659ee888bc9f4
Freezeh11 <freezey789@gmail.com>
2025-09-26 00:45:27 +0800

    made default page to register page

--- qprint/myapp/urls.py
+++ qprint/myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path("", views.register, name="register_root"), 
+    path("register/", views.register, name="register"), 
     path("login/", views.login_view, name="login"),
     path("verify/", views.verify, name="verify"),
     path("logout/", views.logout_view, name="logout"),
