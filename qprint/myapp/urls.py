9dd5bf4fe8d76fe5677866d6ab2109f3f2c567ec
Freezeh11 <freezey789@gmail.com>
2025-09-30 15:18:28 +0800

    with UI. fixed problems.

--- qprint/myapp/urls.py
+++ qprint/myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.register, name="register_root"), 
     path("register/", views.register, name="register"), 
-    path("login/", views.login_view, name="login"),
+    path("login/", views.login, name="login"),
     path("verify/", views.verify, name="verify"),
-    path("logout/", views.logout_view, name="logout"),
+    path("logout/", views.logout, name="logout"),
 ]
--- qprint/qprint/settings.py
+++ qprint/qprint/settings.py
@@ -130,4 +130,16 @@ EMAIL_HOST_USER = "freezey789@gmail.com"
 EMAIL_HOST_PASSWORD = "ayesvfkcfvcnhfrl"  
 DEFAULT_FROM_EMAIL = "freezey789@gmail.com"  
 
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.office365.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "qprintteam@outlook.com"
+# EMAIL_HOST_PASSWORD = "wxrlniuqhycathfm"
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
 
