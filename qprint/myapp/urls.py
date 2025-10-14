5a4beb68f761827d3cb439126b51a0ae5228ce92
Freezeh11 <freezey789@gmail.com>
2025-10-14 18:37:36 +0800

    Added staff and student dashboard from Kurt's branch. Implemented role-based login redirection.

--- qprint/myapp/urls.py
+++ qprint/myapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("verify/", views.verify, name="verify"),
     path("logout/", views.logout, name="logout"),
+    path("staff_dashboard/", views.staff_dashboard, name="staff_dashboard"),
+    path("student_dashboard/", views.student_dashboard, name="student_dashboard"),
 ]
