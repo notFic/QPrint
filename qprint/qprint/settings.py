6d45e29537e319e0894931a3a49bc4d0dbe393e7
Freezeh11 <freezey789@gmail.com>
2025-10-19 14:33:59 +0800

    Implement password reset with token

--- qprint/qprint/settings.py
+++ qprint/qprint/settings.py
@@ -138,8 +138,6 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
-
-
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
