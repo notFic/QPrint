f6e85279a01ec3f82f095000e4ed5aebfd12156a
Freezeh11 <freezey789@gmail.com>
2025-11-01 15:09:47 +0800

    Added branch for development purposes

--- qprint/file_handling/apps.py
+++ qprint/file_handling/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FileHandlingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'qprint.file_handling'
+    name = 'file_handling'
