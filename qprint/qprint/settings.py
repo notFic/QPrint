8e93092e49bfec003eab9333f78a60d8d2a65084
Freezeh11 <freezey789@gmail.com>
2025-11-29 20:22:56 +0800

    Test for render (2)

--- qprint/qprint/settings.py
+++ qprint/qprint/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n*st0-^)tqsw9)6iia47zwln^1m5de)csg=t3#mll9=$0_lnc-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'qprint.onrender.com']
 
